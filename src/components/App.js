bca66cb380961631dcf9c6861eac243f814a7973
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-07 17:10:29 -0200

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Header from './header';
+
 class App extends Component {
   render() {
     return (
       <div className="container">
-        Hoc!
+        <Header />
       </div>
     );
   }
